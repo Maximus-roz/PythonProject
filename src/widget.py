dc3c7418330924bd6b90e2b0cbf06c0090e15d2c
Maksim Belik <rozysknik@mail.ru>
2026-08-03 16:42:48 +0700

    проверка flake8 и правка black

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
 from masks import get_mask_card_number, get_mask_account
 
+
 def mask_account_card(account_number=""):
     if not account_number:  # Проверка на пустую строку
         return "Аргумент не передан"
@@ -7,7 +8,7 @@ def mask_account_card(account_number=""):
     account_number_int = None
     account_number_str = []
     substring_counter = 0
-    for number in account_number_spl:    # разделение на строковые и цифровые значения
+    for number in account_number_spl:  # разделение на строковые и цифровые значения
         if number.isdigit():
             account_number_int = number
             substring_counter += 1
@@ -15,19 +16,15 @@ def mask_account_card(account_number=""):
             account_number_str.append(number)
             substring_counter += 1
 
-    if substring_counter > 3: # защита от большего чем 2 количества подстрок
+    if substring_counter > 3:  # защита от большего чем 2 количества подстрок
         return account_number
 
-
-
-    account_number_str_join =" ".join(account_number_str)
+    account_number_str_join = " ".join(account_number_str)
 
     if len(account_number_int) == 20:
         result = get_mask_account(account_number_int)
         print(f"{account_number_str_join} {result}")
         return f"{account_number_str_join} {result}"
-
-
 
     elif len(account_number_int) == 16:
         result = get_mask_card_number(account_number_int)
@@ -38,14 +35,4 @@ def mask_account_card(account_number=""):
         return account_number
 
 
-
-
-
 mask_account_card()
-
-
-
-
-    
-
-
